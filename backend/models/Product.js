477554aeb1ae16daf7033cbf8ac35bf2e4688a85
ff <tsturjo57@gmail.com>
2026-01-20 18:26:51 +0600

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,12 +1,10 @@
-
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  image: String,
-  category: String
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  inStock: { type: Boolean, default: true } // নতুন যোগ করা হয়েছে
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
